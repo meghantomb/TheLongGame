ddecde068f738dc9a4cca6fa0ed82f1c2dc692af
Meghan <meghantomback@gmail.com>
2025-11-23 18:11:28 -0500

    cool

---
+++ TheLongGame/TheLongGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheLongGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string playerName = Console.ReadLine();
+            string path = "" + playerName + ".txt";
+            int score = 0;
+            try
+            {
+                StreamReader reader = new StreamReader(path);
+                while (!reader.EndOfStream)
+                {
+                    score = int.Parse(reader.ReadLine());
+                }
+                reader.Close();
+                Console.WriteLine("welcome back " + playerName);
+
+            }
+            catch (FileNotFoundException e)
+            {
+                score = 0;
+            }
+            Console.WriteLine("starting score: " + score);
+            Console.WriteLine("start hitting keys!");
+            var nextKey = Console.ReadKey().Key;
+            while(nextKey != ConsoleKey.Enter)
+            {
+                score += 1;
+                Console.WriteLine("\ncurrent score: " + score);
+                nextKey = Console.ReadKey().Key;
+            }
+
+            File.AppendAllText(path, "" + score);
+        }
+    }
+}
